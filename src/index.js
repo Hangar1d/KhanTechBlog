1cab9bc33b39dafd051ce71357183802a9bf7035
KHAN <78674072+Hangar1d@users.noreply.github.com>
2024-05-31 11:01:36 +0900

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react"; 
+import { createRoot } from "react-dom/client"; 
+import { QueryClient, QueryClientProvider } from "react-query"; 
+import { HashRouter } from "react-router-dom"; 
+import App from "./App"; 
+const queryClient = new QueryClient(); 
+const container = document.getElementById("root"); 
+const root = createRoot(container); 
+root.render( 
+  <React.StrictMode>  
+    <HashRouter> 
+      <QueryClientProvider client={queryClient}> 
+        <App /> 
+      </QueryClientProvider> 
+    </HashRouter> 
+  </React.StrictMode> 
+);
